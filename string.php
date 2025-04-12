e751aa95c0700bbfe96294df337fc4457515a57b
Divine Charlotte <divinemaina@gmail.com>
2025-04-12 18:19:03 +0200

    Add files via upload
    string in php

---
+++ string.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration Info</title>
+</head>
+<body>
+    <h1>Student Registration</h1>
+
+    <p>
+        <?php
+            $registrationNumber = "2405001125";
+            $customMessage = "Welcome to the Yearly Registration System!";
+
+            echo "Registration Number: <strong>$registrationNumber</strong><br>";
+            echo "Message: <em>$customMessage</em>";
+        ?>
+    </p>
+</body>
+</html>
