60a66c65d7b48a867a76c1f12cdda5b027e17923
Divine Charlotte <divinemaina@gmail.com>
2025-04-12 18:15:29 +0200

    Add files via upload
    first php

---
+++ me.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP with HTML Example</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+
+    <p>
+        <?php
+            echo "Hello, visitor! Today is " . date("l, F jS, Y");
+        ?>
+    </p>
+</body>
+</html>
